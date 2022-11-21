0d69542129f5c021b356b0d7bdd30014aae109a9
Ale-1407 <alessiacarrera07@gmail.com>
2022-11-21 16:12:14 +0100

    stampa email  in console

--- js/main.js
+++ js/main.js
@@ -13,15 +13,20 @@ var app = new Vue ({
       
     },
     mounted() {
-  
-    },
+        for(i = 0; i < 10; i++){
+          axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+          .then(function (response) {
+          console.log(response.data.response);
+        })
+        }
+    },   
     created() {
   
     },
-    beforeUpdate() {
-  
+    beforeUpdate(){
+
     },
     methods: {
-        
+
     }
 })
