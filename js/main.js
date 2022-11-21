d89d94162338e8895d0193f858173c0b3f77c73f
Ale-1407 <alessiacarrera07@gmail.com>
2022-11-21 16:29:57 +0100

    da correggere stampa in pagina

--- js/main.js
+++ js/main.js
@@ -10,14 +10,16 @@ tutti generati.
 var app = new Vue ({
     el: '#app',
     data: {
-      
+      email: '',
     },
     mounted() {
         for(i = 0; i < 10; i++){
           axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
           .then(function (response) {
           console.log(response.data.response);
-        })
+          const result = response.data.response;
+          this.email = result;
+        });
         }
     },   
     created() {
