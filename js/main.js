53ee911ff56339af047494cfd15dec65e19c71b3
Ale-1407 <alessiacarrera07@gmail.com>
2022-11-21 15:48:00 +0100

    struttura file

---
+++ js/main.js
@@ -0,0 +1,27 @@
+/*
+Generare 10 indirizzi email e stamparli in pagina all'interno 
+di una lista.
+
+Bonus
+Far comparire gli indirizzi email solamente quando sono stati 
+tutti generati.
+*/
+
+var app = new Vue ({
+    el: '#app',
+    data: {
+      
+    },
+    mounted() {
+  
+    },
+    created() {
+  
+    },
+    beforeUpdate() {
+  
+    },
+    methods: {
+        
+    }
+})
